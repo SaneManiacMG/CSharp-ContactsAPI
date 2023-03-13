41eb268eab151407333c2be39f480e172a28371e
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-03-13 05:00:46 +0200

    Created RestGet method to get all users

---
+++ Clients-API/Clients-API/Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Clients_API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clients_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
+        }
+    }
+}
