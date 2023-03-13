9519b9f4deb9dbc2c450e96d5f48eba0df90c3e5
Mogomotsi Moroane <mmoroane@hotmail.com>
2023-03-13 04:48:51 +0200

    Created DBContext

--- Clients-API/Clients-API/Data/ContactsAPIDbContext.cs
+++ Clients-API/Clients-API/Data/ContactsAPIDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Clients_API.Data
+﻿using Clients_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clients_API.Data
 {
-    public class ContactsAPIDbContext
+    public class ContactsAPIDbContext : DbContext
     {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
